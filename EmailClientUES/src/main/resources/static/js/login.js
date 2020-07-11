6c12714d603245819e2631a710a39cfb0262d647
ljubica_m56 <ljubica.m19@gmail.com>
2020-07-11 21:30:52 +0200

    Neke izmene

--- EmailClientUES/src/main/resources/static/js/login.js
+++ EmailClientUES/src/main/resources/static/js/login.js
@@ -1,4 +1,29 @@
-function login() {
+//function login() {
+//	
+//	var usernameInput = $('#usernameInput');
+//	var passwordInput = $('#passwordInput');
+//	
+//	var username = usernameInput.val();
+//	var password = passwordInput.val();
+//	
+//	$.post("http://localhost:8080/api/users/user/loginUser",
+//		{
+//			'username':username,
+//			'password':password
+//		
+//		},
+//	function (response) {
+//			console.log('login');
+//			console.log(response);
+//			alert('Successful login');
+//			
+//			window.location.href = 'messages.html'
+//		}
+//			
+//	
+//	);
+//}
+function login(){
 	
 	var usernameInput = $('#usernameInput');
 	var passwordInput = $('#passwordInput');
@@ -6,20 +31,41 @@ function login() {
 	var username = usernameInput.val();
 	var password = passwordInput.val();
 	
-	$.post("http://localhost:8080/api/users/user/loginUser",
-		{
-			'username':username,
-			'password':password
-		
-		},
-	function (response) {
-			console.log('login');
-			console.log(response);
-			alert('Successful login');
-			
-			window.location.href = 'messages.html'
-		}
-			
+//	$.post("http://localhost:8080/api/users/user/login",
+//			
+//	{
+//		'username': username, 
+//		'password': password
+//	},
+//	function(response){
+//		console.log('login');
+//		console.log(response);
+//		alert('Uspesno ste se ulogovali');
+//		
+//		if(response == 'Admin'){
+//			window.location.href = 'admin.html';
+//		}else {
+//			window.location.href = 'regular.html';
+//		}
+//	}
+//	);
 	
-	);
+	$.ajax({
+		url: 'http://localhost:8080/api/users/user/login.html',
+		type: 'post',
+		data: {'username':username, 'password':password},
+		success:function(response){
+			alert("Login uspesan");
+			console.log('Successful request');
+//			if(response == 'Admin'){
+//				window.location.href = 'admin.html';
+//			}else {
+//				window.location.href = 'regular.html';
+//			}
+		}
+	}).fail(function(xhr, err) {
+		console.log('Error request');
+		console.log(err);
+		alert("Error");
+	});
 }
